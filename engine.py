b966582e74fbacc8fcbd9d74d203ba412cbcd941
Josh Balch <Joshrbalch@gmail.com>
2024-03-04 22:29:52 -0600

    Game works in window!

--- engine.py
+++ engine.py
@@ -1,15 +1,21 @@
 import chess
 import chess.svg
+import random
 
 class Engine:
-    def __init__(self):
-        self.board = chess.Board()
+    def __init__(self, board):
+        self.board = board
         self.chessboardSvg = chess.svg.board(self.board).encode("UTF-8")
 
     def move(self, move):
         self.board.push(chess.Move.from_uci(move))
         self.chessboardSvg = chess.svg.board(self.board).encode("UTF-8")
         return self.chessboardSvg
+    
+    def randomMove(self, board):
+        move = random.choice(list(board.legal_moves))
+        board.push(move)
+        return board
     
     def get_board(self):
         return self.board
--- main.py
+++ main.py
@@ -1,52 +1,47 @@
 import chess
 import chess.svg
-
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
-
 from engine import Engine
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setGeometry(50, 50, 550, 550)
-
         self.widgetSvg = QSvgWidget(parent=self)
         self.widgetSvg.setGeometry(5, 5, 540, 540)
-
         self.chessboard = chess.Board()
-
-        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
-        self.widgetSvg.load(self.chessboardSvg)
-
-    def paintEvent(self, board):
-         self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
-         self.widgetSvg.load(self.chessboardSvg) 
-
-    def setBoard(self, board):
-        self.chessboard = board
+        self.renderBoard()
 
     def renderBoard(self):
         self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
         self.widgetSvg.load(self.chessboardSvg)
 
-if __name__ == "__main__":
-    engine = Engine()
-    # app = QApplication([])
-    # window = MainWindow()
-    # window.show()
-    # app.exec()
+    def setBoard(self, board):
+        self.chessboard = board
+        self.renderBoard()
 
-    while(1):
-        board = engine.get_board()
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+
+    board = chess.Board()
+    engine = Engine(board)
+
+    while not board.is_game_over():
         move = input("Enter a move: ")
+        if move == "exit":
+            break
+    
         if not board.is_legal(chess.Move.from_uci(move)):
             print("Invalid move")
             continue
 
         engine.move(move)
-        print(engine.get_board())
-        print("\n")
-    # engine.move(move)
-    # window.setBoard(engine.get_board())
+        window.setBoard(board)
+
+        board = engine.randomMove(board)
+        window.setBoard(board)
+
+    app.exec()
